ee62389348ad5b6cf4b6fcab1869dd3dfa360d43
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-26 15:23:44 +0200

    [UPD] inserted into the migration file the columns

--- database/migrations/2024_09_26_125535_create_trains_table.php
+++ database/migrations/2024_09_26_125535_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 50);
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
             $table->timestamps();
         });
     }
